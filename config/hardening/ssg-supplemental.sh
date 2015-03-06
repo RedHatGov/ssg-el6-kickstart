05ff58a22d89eba1c75b67006148df820ab7906d
Frank Caviggia <fcaviggia@users.noreply.github.com>
2015-03-05 23:25:52 -0500

    Filesystem Attributes
    Update for filesystem attributes.

--- config/hardening/ssg-supplemental.sh
+++ config/hardening/ssg-supplemental.sh
@@ -111,6 +111,64 @@ ALL: ALL
 EOF
 
 ########################################
+# Filesystem Attributes
+#  CCE-26499-4,CCE-26720-3,CCE-26762-5,
+#  CCE-26778-1,CCE-26622-1,CCE-26486-1.
+#  CCE-27196-5
+########################################
+FSTAB=/etc/fstab
+SED=`which sed`
+
+if [ $(grep " \/sys " ${FSTAB} | grep -c "nosuid") -eq 0 ]; then
+	MNT_OPTS=$(grep " \/sys " ${FSTAB} | awk '{print $4}')
+	${SED} -i "s/\( \/sys.*${MNT_OPTS}\)/\1,nodev,nosuid/" ${FSTAB}
+fi
+if [ $(grep " \/boot " ${FSTAB} | grep -c "nosuid") -eq 0 ]; then
+	MNT_OPTS=$(grep " \/boot " ${FSTAB} | awk '{print $4}')
+	${SED} -i "s/\( \/boot.*${MNT_OPTS}\)/\1,nodev,nosuid/" ${FSTAB}
+fi
+if [ $(grep " \/usr " ${FSTAB} | grep -c "nodev") -eq 0 ]; then
+	MNT_OPTS=$(grep " \/usr " ${FSTAB} | awk '{print $4}')
+	${SED} -i "s/\( \/usr .*${MNT_OPTS}\)/\1,nodev,nosuid/" ${FSTAB}
+fi
+if [ $(grep " \/home " ${FSTAB} | grep -c "nodev") -eq 0 ]; then
+	MNT_OPTS=$(grep " \/home " ${FSTAB} | awk '{print $4}')
+	${SED} -i "s/\( \/home .*${MNT_OPTS}\)/\1,nodev,nosuid/" ${FSTAB}
+fi
+if [ $(grep " \/export\/home " ${FSTAB} | grep -c "nodev") -eq 0 ]; then
+	MNT_OPTS=$(grep " \/export\/home " ${FSTAB} | awk '{print $4}')
+	${SED} -i "s/\( \/export\/home .*${MNT_OPTS}\)/\1,nodev,nosuid/" ${FSTAB}
+fi
+if [ $(grep " \/usr\/local " ${FSTAB} | grep -c "nodev") -eq 0 ]; then
+	MNT_OPTS=$(grep " \/usr\/local " ${FSTAB} | awk '{print $4}')
+	${SED} -i "s/\( \/usr\/local.*${MNT_OPTS}\)/\1,nodev,nosuid/" ${FSTAB}
+fi
+if [ $(grep " \/dev\/shm " ${FSTAB} | grep -c "nodev") -eq 0 ]; then
+	MNT_OPTS=$(grep " \/dev\/shm " ${FSTAB} | awk '{print $4}')
+	${SED} -i "s/\( \/dev\/shm.*${MNT_OPTS}\)/\1,nodev,noexec,nosuid/" ${FSTAB}
+fi
+if [ $(grep " \/tmp " ${FSTAB} | grep -c "nodev") -eq 0 ]; then
+	MNT_OPTS=$(grep " \/tmp " ${FSTAB} | awk '{print $4}')
+	${SED} -i "s/\( \/tmp.*${MNT_OPTS}\)/\1,nodev,noexec,nosuid/" ${FSTAB}
+fi
+if [ $(grep " \/var\/tmp " ${FSTAB} | grep -c "nodev") -eq 0 ]; then
+	MNT_OPTS=$(grep " \/var\/tmp " ${FSTAB} | awk '{print $4}')
+	${SED} -i "s/\( \/var\/tmp.*${MNT_OPTS}\)/\1,nodev,noexec,nosuid/" ${FSTAB}
+fi
+if [ $(grep " \/var\/log " ${FSTAB} | grep -c "nodev") -eq 0 ]; then
+	MNT_OPTS=$(grep " \/var\/tmp " ${FSTAB} | awk '{print $4}')
+	${SED} -i "s/\( \/var\/tmp.*${MNT_OPTS}\)/\1,nodev,noexec,nosuid/" ${FSTAB}
+fi
+if [ $(grep " \/var\/log\/audit " ${FSTAB} | grep -c "nodev") -eq 0 ]; then
+	MNT_OPTS=$(grep " \/var\/log\/audit " ${FSTAB} | awk '{print $4}')
+	${SED} -i "s/\( \/var\/log\/audit.*${MNT_OPTS}\)/\1,nodev,noexec,nosuid/" ${FSTAB}
+fi
+if [ $(grep " \/var " ${FSTAB} | grep -c "nodev") -eq 0 ]; then
+	MNT_OPTS=$(grep " \/var " ${FSTAB} | awk '{print $4}')
+	${SED} -i "s/\( \/var.*${MNT_OPTS}\)/\1,nodev,nosuid/" ${FSTAB}
+fi
+
+########################################
 # File Ownership 
 ########################################
 find / -nouser -print | xargs chown root
